b779588ef195d5cb78ba551ee00f1adb453a29fb
Jordi Joan <jordi.joan@gmail.com>
2019-09-24 14:35:10 +1200

    Signature1000 added

--- adcp.py
+++ adcp.py
@@ -1,5 +1,3 @@
-import datetime
-import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -7,8 +5,9 @@ import pandas as pd
 import scipy.io as sio
 import seaborn as sns
 import itertools
-from constants import (TIMEZONE, AQUADOPPS_HEIGHTS, ADCP_DATES, DATES_FORMAT,
-                       AQUADOPPS_SENS, AQUADOPPS_LEVELS)
+from constants import (TIMEZONE, AQUADOPPS_HEIGHTS, ADCP_DATES, AQUADOPPS_SENS,
+                       AQUADOPPS_LEVELS, AQUADOPPS_A1_LEVELS,
+                       SIGNATURE1000_HEIGHTS)
 from pandas.plotting import register_matplotlib_converters
 from tools import station
 
@@ -16,13 +15,10 @@ from tools import station
 register_matplotlib_converters()
 
 
-FILEPATH = "D:/FoT_ADCP_data_09052019/Currents/ADCP/"
-BINS = 40
-INTERVAL = 600  # secs
+# FILEPATH = "D:/FoT_ADCP_data_09052019/Currents/ADCP/"
+FILEPATH = "C:/Users/jordi/code/muddy/data/Currents/ADCP/"
 
-VARS = ["a1", "a2", "a3",  # amplitudes
-        "Vel_Mag", "Vel_Dir_TN",  # velocity mag. and dir
-        "Vel_E_TN", "Vel_N_TN", "Vel_Up"]  # velocity components
+INTERVAL = 600  # secs
 
 SEN_HEADERS = [
     "Month",
@@ -45,10 +41,256 @@ SEN_HEADERS = [
 ]
 
 
-class Adcp(object):
+class Signature1000(object):
+    """
+    Represents an Signature1000 ADCP and its associated data
+    """
+    VARS = [
+        # 'Burst_Velocity_ENU',
+        'Burst_Amplitude_Beam',
+        # 'Burst_Correlation_Beam',
+        'IBurst_Velocity_Beam',
+        'IBurst_Amplitude_Beam',
+        'IBurst_Correlation_Beam',
+        # 'Burst_Pressure',
+        # 'Burst_Heading',
+        # 'Burst_Roll',
+        # 'Burst_Temperature',
+        'Vel_E_TN',
+        'Vel_N_TN',
+        'Vel_Mag',
+        'Vel_Dir_TN',
+        # 'Burst_Pitch'
+    ]
+
+    IND_VARS = [
+        'Burst_Heading',
+        'Burst_Pressure',
+        'Burst_Temperature',
+    ]
+
+    AMPLITUDES = ['a1', 'a2', 'a3', 'a4']
+
+    def __init__(self, site):
+        folder = "Site{0}/FoT_Signature1000_S{0}_BurstStats_noQC.mat".format(
+            site)
+        datafile = os.path.join(FILEPATH, folder)
+        ml_data = sio.loadmat(datafile)["BurstStats"]
+        self.site = site
+        # MATLAB datenum to Python
+        timestamps = []
+        timestamps.append(
+            pd.to_datetime(ml_data["Time"][0][0][0][0]-719529, unit='D'))
+        # round to 00.00 secs
+        timestamps[0] = timestamps[0] - pd.Timedelta(
+            microseconds=timestamps[0].microsecond)
+        timestamps[0] = timestamps[0] - pd.Timedelta(
+            nanoseconds=timestamps[0].nanosecond)
+        for i in range(1, len(ml_data["Time"][0][0])):
+            timestamps.append(timestamps[i-1] + pd.Timedelta("%ss" % INTERVAL))
+        # Populate vars
+        df_vars = {}
+        for v in self.VARS:
+            df_vars[v] = []
+        for i in range(0, len(ml_data["Vel_E_TN"][0][0])):
+            for j in range(0, len(ml_data["Vel_E_TN"][0][0][i])):
+                for v in self.VARS:
+                    df_vars[v].append(ml_data[v][0][0][i][j])
+        # Water depth
+        wd = [m[0] for m in ml_data["Burst_Pressure"][0][0]]
+        wt = [m[0] for m in ml_data["Burst_Temperature"][0][0]]
+        wd = pd.DataFrame(
+            {"WaterDepth": wd, 'Temperature': wt},
+            index=timestamps,
+            columns=["WaterDepth", "Temperature"])
+        wd = wd[ADCP_DATES["start"]:ADCP_DATES["end"]]
+        wd.index.name = "Date"
+        wd.index = wd.index.tz_localize(TIMEZONE)
+        self.wd = wd
+        # Main dataframe
+        index = list(itertools.product(timestamps, SIGNATURE1000_HEIGHTS))
+        indx = pd.MultiIndex.from_tuples(index, names=['Date', 'Height'])
+        df = pd.DataFrame(df_vars, indx, self.VARS)
+        df = df.tz_localize(TIMEZONE, level=0)
+        df[self.AMPLITUDES] = pd.DataFrame(
+            df.Burst_Amplitude_Beam.values.tolist(), index=df.index)
+        df = df.drop(columns="Burst_Amplitude_Beam")
+        df = df.loc[ADCP_DATES["start"]:ADCP_DATES["end"]]
+        self.df = df.join(self.wd, how="left")
+        # clean data
+        if AQUADOPPS_A1_LEVELS[self.site - 1] is not None:
+            vars_nan = self.VARS[-(len(self.VARS)-1):] + self.AMPLITUDES
+            self.df.loc[self.df.a1 < AQUADOPPS_A1_LEVELS[self.site - 1],
+                        vars_nan] = np.NaN
+            # self.df["OutOfWater"] = self.df.apply(
+            #     lambda r: r.IBurst_Correlation_Beam < 70,
+            #     axis=1)
+            # self.df.loc[self.df.OutOfWater, vars_nan] = np.NaN
+            # self.df["minHeight"] = self.df.apply(
+            #     lambda r: self.df.loc[
+            #         r.name[0], ].query("OutOfWater == True").index.min(),
+            #     axis=1)
+            # too expensive, not good to iterate on DF and perform updates..
+            # for i in self.df.index.get_level_values(0):
+            #     for j in self.df.loc[i].index.get_level_values(0):
+            #         if self.df.loc[i, j].OutOfWater:
+            #             lower_idx = list(filter(lambda x: x > j,
+            #                              SIGNATURE1000_HEIGHTS))
+            #             self.df.loc[(i, lower_idx), vars_nan] = np.NaN
+
+    def clean_by_hah(self, threshold=1, min_hah=1):
+        # agressive cleanup
+        vars_nan = self.VARS[-(len(self.VARS)-1):] + self.AMPLITUDES
+        lower_idx = list(filter(lambda x: x > min_hah,
+                                SIGNATURE1000_HEIGHTS))
+        self.df["OutOfWater"] = self.df.apply(
+            lambda r: (r.WaterDepth - r.name[1]) < threshold,  # depth - hah
+            axis=1)
+        r_df = self.df.reset_index()
+        # idx = pd.IndexSlice
+        r_df.loc[((r_df.Height > min_hah) & (r_df.OutOfWater)), vars_nan] = np.NaN
+        self.df = r_df.set_index(['Date', 'Height'])
+
+    def clean_by_correlation(self):
+        self.df["minHeight"] = self.df.apply(
+                lambda r: self.df.loc[
+                    r.name[0], ].query("OutOfWater == True").index.min(),
+                axis=1)
+        vars_nan = self.VARS[-(len(self.VARS)-1):] + self.AMPLITUDES
+        df_grouped = self.df.groupby("Date")
+        for d, df in df_grouped:
+            # df[(df.index.get_level_values("Height") > df.minHeight.min()),
+            #     vars_nan] = np.NaN
+            lower_idx = list(filter(lambda x: x > df.minHeight.min(),
+                                    SIGNATURE1000_HEIGHTS))
+            self.df.loc[(d, lower_idx), vars_nan] = np.NaN
+            # self.df = self.df.update(df, overwrite=True)
+
+    def __str__(self):
+        return "Signature1000 %s" % self.site
+
+    def unicode(self):
+        return self.__str__()
+
+    def plot_amplitude(self):
+        for a in ["a1", "a2", "a3", "a4"]:
+            fig, axes = plt.subplots(ncols=1, nrows=4, figsize=(28, 4))
+            cbar_ax = fig.add_axes([0.935, .108, .01, .75])
+            cbar_ax.set_ylabel("Amplitude [counts]")
+            i = 0
+            weeks = self.df.index.get_level_values('Date').week
+            for week, df in self.df.groupby(weeks):
+                ax = axes[i]
+                ax1 = ax.twinx()
+                # Amplitude
+                dfamp = df.reset_index().pivot(columns='Height', index='Date',
+                                               values=a)
+                # uncomment to show nans as 0
+                # .fillna(0)
+                sns.heatmap(
+                    dfamp.transpose(), cmap='RdYlBu_r', vmin=0,
+                    ax=ax, xticklabels=False, yticklabels=37,
+                    linewidths=.0,
+                    cbar=(i == 0),
+                    cbar_ax=cbar_ax if i == 0 else None)
+                ax.invert_yaxis()  # distance from bottom to top
+                ax.set_ylabel("Distance above\nhead [m]")
+                ax.set_ylim(bottom=0)
+                ax.set_xlabel(None)
+                # Water depth
+                wd_df = self.wd[self.wd.index.week == week]
+                wd_df.index = wd_df.index.strftime("%d-%m %H:%M")
+                ax1.plot(wd_df.index, wd_df["WaterDepth"],
+                         color="black", label="Water Depth [m]")
+                ax1.set_ylabel("Water depth [m]")
+                ax1.set_ylim(bottom=0, top=self.wd["WaterDepth"].max())
+                ax1.xaxis.set_ticks(
+                    np.arange(72, wd_df.WaterDepth.count() + 1, 72))
+                ax1.set_xlabel(None)
+                i += 1
+            fig.suptitle("%s - %s" % (str(self), a))
+
+    def plot_magnitude(self):
+        dfwindlist = station.get_weekly_wind()
+        weeks = self.df.index.get_level_values('Date').week
+        i = 0
+        for week, df in self.df.groupby(weeks):
+            dfwind = dfwindlist[i]
+            fig, axes = plt.subplots(ncols=1, nrows=4,
+                                     figsize=(28, 4), sharex=True)
+            ax = axes[0]
+            ax.scatter(dfwind.index, dfwind["speed"], s=4, c="black",
+                       label="Wind speed\n[m/s]")
+            ax.set_ylabel("Wind speed\n[m/s]")
+            ax.set_yticks([0, 7, 14])
+            ax.spines["left"].set_bounds(0, 14)
+            ax.legend(loc="center left",
+                      bbox_to_anchor=(-0.155, 0.65), frameon=False)
+            ax = ax.twinx()
+            ax.scatter(dfwind.index, dfwind["direction"],
+                       marker="x", label="Wind direction\n[degrees]")
+            ax.set_ylabel("Wind direction\n[degrees]")
+            ax.set_yticks([0, 90, 180, 270, 360])
+            ax.legend(loc="center left",
+                      bbox_to_anchor=(-0.155, 0.35), frameon=False)
+            ax.spines["right"].set_bounds(0, 360)
+            ax.xaxis.set_visible(False)
+            ax.spines["right"].set_position(("outward", 15))
+            ax.spines["top"].set_visible(False)
+            ax.spines["bottom"].set_visible(False)
+            ax.spines["left"].set_visible(False)
+            # Temperature
+            ax = axes[1]
+            df_sen = self.wd[self.wd.index.week == week]
+            ax.plot(df_sen.index, df_sen["Temperature"])
+            ax.set_ylabel("Temperature [°C]")
+            # Water depth
+            ax = ax.twinx()
+            ax.plot(df_sen.index, df_sen["WaterDepth"], linestyle=":",
+                    color="black", label="Water Depth [m]")
+            ax.set_ylabel("Water depth [m]")
+            ax.set_ylim(bottom=0, top=self.wd["WaterDepth"].max())
+            # Velocity magnitude & direction
+            ax = axes[2]
+            ax1 = axes[3]
+            levels = AQUADOPPS_LEVELS[self.site - 1]
+            subdf = df.loc[pd.IndexSlice[:, levels[0]], :]
+            subdf.index = subdf.index.droplevel(level=1)  # drop height
+            ax.plot(subdf.index, subdf["Vel_Mag"],
+                    color="blue", label='%.1f mab' % levels[0])
+            ax1.scatter(subdf.index, subdf["Vel_Dir_TN"],
+                        color="blue", label='%.1f mab' % levels[0], s=3)
+            subdf = df.loc[pd.IndexSlice[:, levels[1]], :]
+            subdf.index = subdf.index.droplevel(level=1)  # drop height
+            ax.plot(subdf.index, subdf["Vel_Mag"],
+                    color="red", label='%.1f mab' % levels[1])
+            ax1.scatter(subdf.index, subdf["Vel_Dir_TN"],
+                        color="red", label='%.1f mab' % levels[1], s=3)
+            subdf = df.loc[pd.IndexSlice[:, levels[2]], :]
+            subdf.index = subdf.index.droplevel(level=1)  # drop height
+            ax.plot(subdf.index, subdf["Vel_Mag"],
+                    color="green", label='%.1f mab' % levels[2])
+            ax1.scatter(subdf.index, subdf["Vel_Dir_TN"],
+                        color="green", label='%.1f mab' % levels[2], s=3)
+            ax.legend(loc="center left",
+                      bbox_to_anchor=(-0.155, 0.65), frameon=False)
+            ax1.legend(loc="center left",
+                       bbox_to_anchor=(-0.155, 0.65), frameon=False)
+            ax.set_ylabel("Velocity\nspeed [m/s]")
+            ax1.set_ylabel("Velocity\ndir. [°C]")
+            fig.suptitle("%s - Week %s" % (str(self), i + 1))
+            i += 1
+
+
+class Aquadopp(object):
     """
     Represents an Aquadopp ADCP and its associated data
     """
+
+    VARS = ["a1", "a2", "a3",  # amplitudes
+            "Vel_Mag", "Vel_Dir_TN",  # velocity mag. and dir
+            "Vel_E_TN", "Vel_N_TN", "Vel_Up"]  # velocity components
+
     def __init__(self, site):
         self.site = site
         folder = "Site{0}/Processed/Currents_S{0}_noQC.mat".format(site)
@@ -62,11 +304,11 @@ class Adcp(object):
             timestamps.append(timestamps[i-1] + pd.Timedelta("%ss" % INTERVAL))
         # Populate vars
         df_vars = {}
-        for v in VARS:
+        for v in self.VARS:
             df_vars[v] = []
         for i in range(0, len(ml_data["a1"])):
             for j in range(0, len(ml_data["a1"][i])):
-                for v in VARS:
+                for v in self.VARS:
                     df_vars[v].append(ml_data[v][i][j])
         # Water depth
         wd = [m[0] for m in ml_data["WaterDepth"]]
@@ -80,13 +322,19 @@ class Adcp(object):
         # Main dataframe
         index = list(itertools.product(timestamps, AQUADOPPS_HEIGHTS))
         indx = pd.MultiIndex.from_tuples(index, names=['Date', 'Height'])
-        df = pd.DataFrame(df_vars, indx, VARS)
+        df = pd.DataFrame(df_vars, indx, self.VARS)
         df = df.tz_localize(TIMEZONE, level=0)
         df = df.loc[ADCP_DATES["start"]:ADCP_DATES["end"]]
         self.df = df.join(self.wd, how="left")
-        self.df["OutOfWater"] = self.df.apply(
-                lambda r: (r.WaterDepth - r.name[1]) <= 0.1,
+        # clean data based on amplitude threshold
+        if AQUADOPPS_A1_LEVELS[self.site - 1] is not None:
+            self.df.loc[self.df.a1 < AQUADOPPS_A1_LEVELSS[self.site - 1],
+                        self.VARS] = np.NaN
+        if self.site == 3:  # clean based on hah
+            self.df["OutOfWater"] = self.df.apply(
+                lambda r: (r.WaterDepth - r.name[1]) < 0.8,  # depth - hah
                 axis=1)
+            self.df.loc[self.df.OutOfWater, self.VARS] = np.NaN
         # Sen file (temp, etc..)
         sen_folder = "Site{0}/{1}.sen".format(site, AQUADOPPS_SENS[site - 1])
         sen_datafile = os.path.join(FILEPATH, sen_folder)
@@ -106,7 +354,7 @@ class Adcp(object):
         return self.__str__()
 
     def plot_amplitude(self):
-        for a in VARS[:3]:
+        for a in self.VARS[:3]:
             fig, axes = plt.subplots(ncols=1, nrows=4, figsize=(28, 4))
             cbar_ax = fig.add_axes([0.935, .108, .01, .75])
             cbar_ax.set_ylabel("Amplitude [counts]")
@@ -144,7 +392,7 @@ class Adcp(object):
             fig.suptitle("%s - %s" % (str(self), a))
 
     def plot_velocity(self):
-        for v in VARS[-3:]:
+        for v in self.VARS[-3:]:
             fig, axes = plt.subplots(ncols=1, nrows=4, figsize=(28, 4))
             cbar_ax = fig.add_axes([0.935, .108, .01, .75])
             cbar_ax.set_ylabel("Velocity [m/s]")
@@ -187,20 +435,23 @@ class Adcp(object):
         i = 0
         for week, df in self.df.groupby(weeks):
             dfwind = dfwindlist[i]
-            fig, axes = plt.subplots(ncols=1, nrows=4, figsize=(28, 4), sharex=True)
+            fig, axes = plt.subplots(ncols=1, nrows=4,
+                                     figsize=(28, 4), sharex=True)
             ax = axes[0]
             ax.scatter(dfwind.index, dfwind["speed"], s=4, c="black",
                        label="Wind speed\n[m/s]")
             ax.set_ylabel("Wind speed\n[m/s]")
             ax.set_yticks([0, 7, 14])
             ax.spines["left"].set_bounds(0, 14)
-            ax.legend(loc="center left", bbox_to_anchor=(-0.155, 0.65), frameon=False)
+            ax.legend(loc="center left",
+                      bbox_to_anchor=(-0.155, 0.65), frameon=False)
             ax = ax.twinx()
             ax.scatter(dfwind.index, dfwind["direction"],
                        marker="x", label="Wind direction\n[degrees]")
             ax.set_ylabel("Wind direction\n[degrees]")
             ax.set_yticks([0, 90, 180, 270, 360])
-            ax.legend(loc="center left", bbox_to_anchor=(-0.155, 0.35), frameon=False)
+            ax.legend(loc="center left",
+                      bbox_to_anchor=(-0.155, 0.35), frameon=False)
             ax.spines["right"].set_bounds(0, 360)
             ax.xaxis.set_visible(False)
             ax.spines["right"].set_position(("outward", 15))
@@ -212,6 +463,14 @@ class Adcp(object):
             df_sen = self.df_sen[self.df_sen.index.week == week]
             ax.plot(df_sen.index, df_sen["Temperature"])
             ax.set_ylabel("Temperature [°C]")
+            # Water depth
+            ax = ax.twinx()
+            wd_df = self.wd[self.wd.index.week == week]
+            # wd_df.index = wd_df.index.strftime("%d-%m %H:%M")
+            ax.plot(wd_df.index, wd_df["WaterDepth"], linestyle=":",
+                    color="black", label="Water Depth [m]")
+            ax.set_ylabel("Water depth [m]")
+            ax.set_ylim(bottom=0, top=self.wd["WaterDepth"].max())
             # Velocity magnitude & direction
             ax = axes[2]
             ax1 = axes[3]
@@ -234,8 +493,10 @@ class Adcp(object):
                     color="green", label='%.1f mab' % levels[2])
             ax1.scatter(subdf.index, subdf["Vel_Dir_TN"],
                         color="green", label='%.1f mab' % levels[2], s=3)
-            ax.legend(loc="center left", bbox_to_anchor=(-0.155, 0.65), frameon=False)
-            ax1.legend(loc="center left", bbox_to_anchor=(-0.155, 0.65), frameon=False)
+            ax.legend(loc="center left",
+                      bbox_to_anchor=(-0.155, 0.65), frameon=False)
+            ax1.legend(loc="center left",
+                       bbox_to_anchor=(-0.155, 0.65), frameon=False)
             ax.set_ylabel("Velocity\nspeed [m/s]")
             ax1.set_ylabel("Velocity\ndir. [°C]")
             fig.suptitle("%s - Week %s" % (str(self), i + 1))
--- constants.py
+++ constants.py
@@ -499,9 +499,57 @@ AQUADOPPS_HEIGHTS = [
     4.20
 ]
 
+SIGNATURE1000_HEIGHTS = [
+    0.6,
+    0.8,
+    1.0,
+    1.2,
+    1.4,
+    1.6,
+    1.8,
+    2.0,
+    2.2,
+    2.4,
+    2.6,
+    2.8,
+    3.0,
+    3.2,
+    3.4,
+    3.6,
+    3.8,
+    4.0,
+    4.2,
+    4.4,
+    4.6,
+    4.8,
+    5.0,
+    5.2,
+    5.4,
+    5.6,
+    5.8,
+    6.0,
+    6.2,
+    6.4,
+    6.6,
+    6.8,
+    7.0,
+    7.2,
+    7.4,
+    7.6,
+    7.8,
+    8.0,
+]
+
 AQUADOPPS_SENS = ["A243401", "EXTIKI02", "HR02"]
 AQUADOPPS_LEVELS = [
     [0.3, 0.5, 0.7],  # site 1
     [0.3, 0.6, 1.1],  # site 2
-    [0.3, 1.2, 2.5]  # site 3
+    [0.3, 1, 2],  # site 3
+    [0.6, 1.4, 3]  # site 4
+]
+AQUADOPPS_A1_LEVELS = [  # anything with a1 lower than these values is removed
+    80,
+    None,
+    60,
+    55
 ]
